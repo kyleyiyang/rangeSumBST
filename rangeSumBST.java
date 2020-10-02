e0ebfca45300a94e6ecbd266fde1c2641514941d
kyleyiyang <kyleyiyang@gmail.com>
2020-10-02 00:17:37 -0400

    Create rangeSumBST.java

---
+++ rangeSumBST.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        
+            if (root.val <= R && root.val >= L) {
+                sum = sum + root.val;
+            }
+            if (root.left!=null) {
+                rangeSumBST(root.left, L, R);
+            }
+            if (root.right!=null) {
+                rangeSumBST(root.right, L, R);
+            }
+        return sum;
+    }
+    
+}
