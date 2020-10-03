a35cb3a6027d54516a7a4238ea17b14cfaeedbde
kyleyiyang <kyleyiyang@gmail.com>
2020-10-02 22:15:11 -0400

    Update rangeSumBST.java

--- rangeSumBST.java
+++ rangeSumBST.java
@@ -28,5 +28,26 @@ class Solution {
             }
         return sum;
     }
-    
+}
+// very fast
+
+class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        
+            if (root.val <= R && root.val >= L) {
+                sum = sum + root.val;
+                if (root.left!=null) {
+                    rangeSumBST(root.left, L, R);
+                }
+                if (root.right!=null) {
+                    rangeSumBST(root.right, L, R);
+                }
+            } else if (root.left!=null && root.val > R) {
+                rangeSumBST(root.left, L, R);
+            } else if (root.right!=null && root.val < L) {
+                rangeSumBST(root.right, L, R);
+            }
+        return sum;
+    }
 }
